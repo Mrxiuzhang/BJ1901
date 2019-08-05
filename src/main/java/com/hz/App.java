015f46814b78bb913e8851ce08fa207150a2ba3c
Mrxiuzhang <laopo5203344>
2019-08-05 14:10:05 +0800

    xiu张

--- src/main/java/com/hz/App.java
+++ src/main/java/com/hz/App.java
@@ -6,8 +6,11 @@ package com.hz;
  */
 public class App 
 {
+
     public static void main( String[] args )
     {
-        System.out.println( "6666 Worl   d!" );
+        System.out.println( "6666 Worl d!" );
+
     }
+
 }
